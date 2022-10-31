89742535e645b8958ac6a979fcf0a78fe6dad8b9
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-10-31 17:23:09 +0530

    updating-completed

--- JS/Script.js
+++ JS/Script.js
@@ -1,1 +1,48 @@
 `use strict`;
+//what to do:
+//need to render text inside an array.
+//need to create and element eg: div, li
+//need to render inside the HTMl page.
+
+
+//global variables
+const iteamsEl = document.getElementById(`iteams`)
+const containerEl = document.getElementById('container')
+
+//array
+const NewList = []
+
+//Functions:-
+
+//updating array
+function Updating(){
+    NewList.push(iteamsEl.value)
+    iteamsEl.value = ''
+}
+
+
+// rendering in to page
+function pageRender(){
+    Newli = document.createElement('li')
+    containerEl.appendChild(Newli)
+    NewList.forEach(elem =>{
+        if (elem != ''){
+            Newli.innerText = elem   
+            console.log(NewList)
+        }
+    })
+     
+}
+
+
+
+
+// Task functioning area
+function render(){
+    Updating()
+    pageRender()
+    
+    
+    
+}
+
