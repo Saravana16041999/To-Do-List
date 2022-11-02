ba352b50dd4b8338d3441c1ffdcb7361154115fc
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-11-02 22:38:34 +0530

    completed-first-crud

--- JS/Script.js
+++ JS/Script.js
@@ -1,60 +1,76 @@
 `use strict`;
-//what to do:
-//need to render text inside an array.
-//need to create and element eg: div, li
-//need to render inside the HTMl page.
+const formEl = document.getElementById('Form-list');
+const inputEl = document.getElementById('list');
+const displayareaEl = document.getElementById('display');
 
+const submit = document.getElementById('submit')
+let iteams = [
+    //iteam = {id:1321 value : 'inputEl.value' }
+]
 
-//global variables
-const iteamsEl = document.getElementById(`iteams`)
-const containerEl = document.getElementById('container')
-const renderEl = document.getElementById('renderall') 
-
-
-
-
-
-//array
-let iteams = []
-
-//Functions:-
-
-
-// rendering in to page
-function pageRender(){
-    containerEl.innerHTML = null
+let isEditing = false;
+let editId = null;
+ 
+//function
+function displayUi(){
+    displayareaEl.innerHTML = null;
+    displayareaEl.innerHTML = '<h1>Iteams</h1>'
     if(iteams.length > 0){
-        iteams.forEach(iteam =>{ 
-            const Newli = document.createElement('li')
-            Newli.innerHTML = `${iteam.value}<button class='delete' 
-            onclick= "deletelist(${iteam.id})" >delete</button>`   
-            containerEl.appendChild(Newli)
+        iteams.forEach((iteam)=>{
+            const listEl = document.createElement('li')
+            listEl.classList.add('list_ieam')
+            listEl.innerHTML = `${iteam.value}<button class="dlt_dtn" 
+            onclick="deleteli(${iteam.id})"><i class="fa-solid fa-trash"></i></button>
+            <button class='update' onclick="edting(${iteam.id})"><i class="fa-solid fa-scissors"></button>`;
+            displayareaEl.appendChild(listEl)
         })
-    }      
+    }
+    isEditing = false;
+    editId = null;
+    inputEl.value = null
 }
 
-function deletelist(id){
-    iteams = iteams.filter((iteam)=> console.log(iteam.id !== id))
-    pageRender();
+function deleteli(id){
+    iteams = iteams.filter((iteam)=> iteam.id !== id);
+    displayUi();
 }
 
-// Task functioning area
-renderEl.addEventListener('click',(e)=>{
+function edting(id){
+    const inedit = iteams.find((iteam) => iteam.id === id);
+    // iteams = iteams.filter((iteam)=> iteam.id !== id);
+    inputEl.value = inedit.value
+    editId = id;
+    isEditing = true;
+    
+}
+
+formEl.addEventListener('submit',(e)=>{
     e.preventDefault();
-    if(iteamsEl.value){
-        let iteam ={
-            id :  new Date().valueOf(),
-            value : iteamsEl.value
-        }
-        iteams.push(iteam)
-        // iteamsEl.value = null
-        pageRender()
-    }else{
-        alert('need to Enter some data')
+    if(inputEl.value){
+        if(isEditing){
+            iteams = iteams.map((iteam)=>{
+                if(iteam.id === editId){
+                    return {...iteam, value : inputEl.value};
+                }else{
+                    return iteam;
+                }
+            });
+            displayUi();
+        }else{
+            {
+                const iteam ={
+                    id : new Date().valueOf(),
+                    value : inputEl.value
+                };
+                iteams.push(iteam)
+                displayUi()
+            }
+    }
+}else
+    {
+        alert('enter some thing before adding')
     }
     
-    
-        
-    
-}
-)
+});
+
+//<i class="fa-solid fa-scissors"></i>
