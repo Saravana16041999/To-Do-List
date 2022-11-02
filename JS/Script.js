57a79d0b94341e650c7cb08836f5bdf3f384f0f0
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-11-02 18:44:12 +0530

    adasqweq

--- JS/Script.js
+++ JS/Script.js
@@ -8,39 +8,50 @@
 //global variables
 const iteamsEl = document.getElementById(`iteams`)
 const containerEl = document.getElementById('container')
+const renderEl = document.getElementById('renderall') 
+
+let list ={
+    newID :  new Date().valueOf()
+} 
+
+
 
 //array
-const NewList = []
+let NewList = []
 
 //Functions:-
-
-//updating array
-function Updating(){
+function updatearr(){
     NewList.push(iteamsEl.value)
-    iteamsEl.value = ''
 }
-
 
 // rendering in to page
 function pageRender(){
-    Newli = document.createElement('li')
-    containerEl.appendChild(Newli)
-    NewList.forEach(elem =>{
-        if (elem != ''){
-            Newli.innerText = elem  
-        }
-    })
-     
+    containerEl.innerHTML = null
+    NewList.forEach(elem =>{ 
+            Newli = document.createElement('li')
+            Newli.innerHTML = `${elem}<button class='delete' onclick= "deletelist(${list.newID})" >delete</button>`   
+            containerEl.appendChild(Newli)
+        })
+        
 }
 
-
-
+function deletelist(id){
+}
 
 // Task functioning area
-function render(){
-    Updating()
+renderEl.addEventListener('click',(e)=>{
+    e.preventDefault();
+    
+    //updating array
+    updatearr()
+        
     pageRender()
 }
-
+)
 
 // 
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter((word) => word.length > 6);
+
+console.log(result)
